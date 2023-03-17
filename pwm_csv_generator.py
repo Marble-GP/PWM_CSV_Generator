0c6602e418dc84bff45d0092b1087c3a4e86fb33
S.Watanabe <74084344+Marble-GP@users.noreply.github.com>
2023-03-17 17:47:43 +0900

    Add files via upload

--- pwm_csv_generator.py
+++ pwm_csv_generator.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     file_ref = input("参照する時系列データを入力 :")
     f_carr = float(input("キャリア周波数(Hz) :"))
     T = 1.0/f_carr
-    T_trans = max((T_rise, T_fall))
 
     ref_dat = np.loadtxt(file_ref, delimiter=",").T
     x_ref = ref_dat[1]
@@ -25,11 +24,11 @@ if __name__ == "__main__":
         while t > t_ref[i]:
             i += 1
         
-        if (T_trans+T_dead)/T < x_ref[i] < (T-T_trans-T_dead)/T:
-            OUT_HS.extend([[t+T*x_ref[i], 1], [t+T*x_ref[i]+T_fall, 0], [t+T-T_rise-T_dead, 0], [t+T, 1]])
-            OUT_LS.extend([[t+T*x_ref[i]+T_dead, 0], [t+T*x_ref[i]+T_rise+T_dead, 1], [t+T-T_fall, 1], [t+T, 0]])
+        if (T_rise+T_fall+2*T_dead) < T*x_ref[i] < (T-T_rise-T_fall-2*T_dead):
+            OUT_HS.extend([[t+T*x_ref[i], 1], [t+T*x_ref[i]+T_fall, 0], [t+T-T_rise, 0], [t+T, 1]])
+            OUT_LS.extend([[t+T*x_ref[i]+T_dead, 0], [t+T*x_ref[i]+T_rise+T_dead, 1], [t+T-T_fall-T_dead, 1], [t+T, 0]])
         
-        elif (T_trans+T_dead)/T >= x_ref[i]:
+        elif (T_rise+T_fall+2*T_dead) >= T*x_ref[i]:
             if len(OUT_HS) > 0:
                 OUT_HS[-1][1] = 0
                 OUT_LS[-1][1] = 1
@@ -40,7 +39,7 @@ if __name__ == "__main__":
             OUT_HS.append([t+T, 0])
             OUT_LS.append([t+T, 1])
         
-        elif x_ref[i] >= (T-T_trans-T_dead)/T:
+        elif T*x_ref[i] >= (T-T_rise-T_fall-2*T_dead):
             if len(OUT_HS) > 0:
                 OUT_HS[-1][1] = 1
                 OUT_LS[-1][1] = 0
